fe9a4fdca3491232cf2d2aa3dc2a4cacce76f552
Jaime Morales <jaime@lastrev.com>
2021-06-02 12:07:39 -0700

    got a working prototype

--- packages/graphql-schema-gen/src/fetchers/contentful.ts
+++ packages/graphql-schema-gen/src/fetchers/contentful.ts
@@ -31,20 +31,49 @@ const getFieldType = (typeData: Field | FieldItem): string => {
   }
 };
 
+const reserved: Record<string, string> = {
+  sidekickLookup: 'JSON',
+  id: 'String',
+  theme: 'Theme',
+  animation: 'JSON'
+};
+
 const contentfulFetcher: Fetcher = async (clientParams: CreateClientParams) => {
   const client = createClient(clientParams);
 
   const contentTypes = await client.getContentTypes();
 
+  const warnings: string[][] = [];
+
+  contentTypes.items.forEach((t) => {
+    const warnFields = t.fields.filter((f) => Object.keys(reserved).indexOf(f.id) > -1);
+    if (warnFields.length) {
+      warnings.push(...warnFields.map((w) => [t.sys.id, w.id]));
+    }
+  });
+
+  warnings.forEach((w) =>
+    console.log(
+      `Content Type ${w[0]} is using a reserved field id ${w[1]}. Please make sure that a mapper is written for this field.}`
+    )
+  );
+
   return `
 ${contentTypes.items.map(
   (contentType) => `
 type ${upperFirst(contentType.sys.id)} implements Content {
-  ${contentType.fields.map(
-    (field) => `
-  ${field.id}: ${getFieldType(field)}
+  ${Object.keys(reserved).map(
+    (k) => `
+  ${k}: ${reserved[k]}
   `
   )}
+  ${contentType.fields
+    .filter((t) => Object.keys(reserved).indexOf(t.id) === -1)
+    .map(
+      (field) => `
+  ${field.id}: ${getFieldType(field)}
+  `
+    )}
 }
 `
 )}
