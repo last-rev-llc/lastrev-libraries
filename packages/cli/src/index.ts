0d74226bd8e461308d48a546eb57cb6f54a462cd
Jaime Morales <jaime@lastrev.com>
2021-07-16 09:39:11 -0700

    fixed some issues

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,13 +5,13 @@ program
   .command('gql-serve', 'Run the LastRev GraphQL Server', {
     executableFile: resolve(__dirname, '../dist/gql-serve.js')
   })
-  .command('cms-sync', 'Sync from a CMS to the file system', {
+  .command('cms-sync', 'Sync content files from a CMS to the file system', {
     executableFile: resolve(__dirname, '../dist/cms-sync.js')
   })
   .command('create-app', 'Create an app from one of our examples', {
     executableFile: resolve(__dirname, '../dist/create-app.js')
   })
-  .command('cms-sync-s3', 'Sync conent files from a S3 to the file system', {
+  .command('cms-sync-s3', 'Sync content files from a S3 to the file system', {
     executableFile: resolve(__dirname, '../dist/cms-sync-s3.js')
   })
   .parse(process.argv);
