130762a6d13e82ed1a83d43bbdff81991e516265
Max Techera <maxi.techerag@gmail.com>
2021-08-02 17:27:29 -0300

    Add default for text link

--- packages/component-library/src/components/Text/Text.tsx
+++ packages/component-library/src/components/Text/Text.tsx
@@ -94,7 +94,7 @@ const renderText =
 
 const renderOptions = ({ links }: { links: TextLinks }) => {
   console.log('Text: links', links);
-  const entries = keyBy('id', links?.entries);
+  const entries = keyBy('id', links?.entries ?? []);
   // const assets = keyBy('id', links.assets);
 
   return {
