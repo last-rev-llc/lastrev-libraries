a528c6efe62367864f32ec6f67093c88045a5fef
Dano Alexander <altruity@gmail.com>
2021-08-23 17:22:15 +0300

    Reset mock

--- packages/component-library/src/components/Link/Link.mock.tsx
+++ packages/component-library/src/components/Link/Link.mock.tsx
@@ -2,10 +2,8 @@ import { lorem } from 'faker';
 
 export default {
   __typename: 'Link',
-  // variant: 'button-contained',
-  variant: 'text',
+  variant: 'button-contained',
   href: lorem.word(),
   text: lorem.words(2),
-  // icon: 'instagram'
-  icon: 'caret-right'
+  icon: 'instagram'
 };
