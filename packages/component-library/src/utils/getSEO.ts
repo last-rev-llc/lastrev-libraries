d886930fbefbe497d00236b98340cc53bab4b415
Dano Alexander <altruity@gmail.com>
2021-09-13 18:40:31 +0300

    TS fixes

--- packages/component-library/src/utils/getSEO.ts
+++ packages/component-library/src/utils/getSEO.ts
@@ -1,28 +1,47 @@
 import isPlainObject from 'lodash/isPlainObject';
 
+interface Option {
+  value: string | { url: string };
+  name: any;
+}
+
+interface Options {
+  [key: string]: Option
+}
+
+interface Tag {
+  content: string;
+  property?: string;
+  name?: string;
+}
+
 const getArgs = (args: any[]) => args.filter((arg) => isPlainObject(arg));
 
-const getOptions = (args: any[]) => args.reduce((a, b) => ({ ...b, ...a }), {});
+const getOptions = (args: any[]) => args.reduce((a: any, b: any) => ({ ...b, ...a }), {});
 
-const isOGSimple = (option: { name: string; value: any; }) => option.name.startsWith('og:') && typeof option.value === 'string';
+const isOGSimple = (option: Option) => option.name.startsWith('og:') && typeof option.value === 'string';
 
-const isOGComplex = (option: { name: string; value: { url: any; }; }) => option.name.startsWith('og:') && isPlainObject(option.value) && option.value.url;
+const isOGComplex = (option: Option) => option.name.startsWith('og:') && typeof option.value !== 'string' && option.value.url;
 
-const isTwitter = (option: { name: string; value: any; }) => option.name.startsWith('twitter:') && typeof option.value === 'string';
+const isTwitter = (option: Option) => option.name.startsWith('twitter:') && typeof option.value === 'string';
 
-const isTwitterComplex = (option: { name: string; value: { url: any; }; }) =>
-  option.name.startsWith('twitter:') && isPlainObject(option.value) && option.value.url;
+const isTwitterComplex = (option: Option) =>
+  option.name.startsWith('twitter:') && typeof option.value !== 'string' && option.value.url;
 
-const isComplex = (option: { value: { url: any; }; }) => isPlainObject(option.value) && option.value.url;
+const isComplex = (option: Option) => typeof option.value !== 'string' && option.value.url;
 
-const getTags = (options: { [x: string]: { value: any; }; }) =>
-  Object.keys(options).reduce((acc, key) => {
-    if (isOGSimple(options[key])) acc.push({ property: options[key].name, content: options[key].value });
-    else if (isOGComplex(options[key])) acc.push({ property: options[key].name, content: options[key].value.url });
-    else if (isTwitter(options[key])) acc.push({ name: options[key].name, content: options[key].value });
-    else if (isTwitterComplex(options[key])) acc.push({ name: options[key].name, content: options[key].value.url });
-    else if (isComplex(options[key])) acc.push({ name: options[key].name, content: options[key].value.url });
-    else acc.push({ name: options[key].name, content: options[key].value });
+const getTags = (options: Options ) =>
+  Object.keys(options).reduce((acc: Array<Tag>, key) => {
+    const value = options[key].value;
+    if(typeof value === 'string'){
+      if (isOGSimple(options[key])) acc.push({ property: options[key].name, content: value  });
+      else if (isTwitter(options[key])) acc.push({ name: options[key].name, content: value });
+      else acc.push({ name: options[key].name, content: value });
+    } else {
+       if (isOGComplex(options[key])) acc.push({ property: options[key].name, content: value.url });
+      else if (isTwitterComplex(options[key])) acc.push({ name: options[key].name, content: value.url });
+      else if (isComplex(options[key])) acc.push({ name: options[key].name, content: value.url });
+    }
     return acc;
   }, []);
 
