0a6b8f4b4a5685ea154874a67297a631442afe33
Dano Alexander <altruity@gmail.com>
2021-09-14 12:31:40 +0300

    Fix BackToTop unit test

--- packages/component-library/src/components/BackToTop/BackToTop.test.tsx
+++ packages/component-library/src/components/BackToTop/BackToTop.test.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import preloadAll from 'jest-next-dynamic';
+// import preloadAll from 'jest-next-dynamic';
+// import BackToTop, { BackToTopProps } from './BackToTop';
 import BackToTop from './BackToTop';
 import mockContent from './BackToTop.mock';
 
-beforeAll(async () => {
-  await preloadAll();
-});
+// beforeAll(async () => {
+//   await preloadAll();
+// });
 
-const renderComponent = (mockBackToTop) => {
-  return render(<BackToTop {...mockBackToTop} />);
-};
+const renderComponent = () => render(<BackToTop sidekickLookup={''} {...mockContent} />);
 
 describe('<BackToTop />', () => {
   test('BackToTop renders correctly', () => {
-    const mockBackToTop = mockContent();
-    const { getByTestId } = renderComponent(mockBackToTop);
+    // const mockBackToTop = mockContent();
+    // const { getByTestId } = renderComponent(mockBackToTop);
+    // expect(getByTestId('BackToTop')).toBeDefined();
+    // test.todo('write tests');
+
+    const { getByTestId } = renderComponent();
     expect(getByTestId('BackToTop')).toBeDefined();
   });
 });
