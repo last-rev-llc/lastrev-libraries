aa9a9919896bd78a6139cc3615621ca66d2c50cc
cocowidmer <ceciliawidmer1993@gmail.com>
2021-09-20 10:16:20 -0300

    back to top button will dissapear when scrolling down and updated changeset

---
+++ .changeset/honest-dingos-reply.md
@@ -0,0 +1,5 @@
+---
+'@last-rev/component-library': patch
+---
+
+'updated back to top button to hide when scrolling down'
--- packages/component-library/src/components/BackToTop/BackToTop.tsx
+++ packages/component-library/src/components/BackToTop/BackToTop.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Fab, { FabProps } from '@material-ui/core/Fab';
+import Box from '@material-ui/core/Box';
 import { styled } from '@material-ui/core';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import ErrorBoundary from '../ErrorBoundary';
 import sidekick from '../../utils/sidekick';
 
@@ -15,12 +17,15 @@ export const BackToTop = ({ FabProps, sidekickLookup }: BackToTopProps) => {
   const handleClick = () => {
     window.scrollTo({ top: 0, left: 0 });
   };
+  const trigger = useScrollTrigger();
 
   return (
     <ErrorBoundary>
-      <Root {...FabProps} onClick={handleClick} data-testid="BackToTop" {...sidekick(sidekickLookup)}>
-        <KeyboardArrowUpIcon />
-      </Root>
+      <Box display={trigger ? 'none' : 'flex'}>
+        <Root {...FabProps} onClick={handleClick} data-testid="BackToTop" {...sidekick(sidekickLookup)}>
+          <KeyboardArrowUpIcon />
+        </Root>
+      </Box>
     </ErrorBoundary>
   );
 };
