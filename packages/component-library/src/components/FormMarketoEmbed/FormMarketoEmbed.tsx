89bda2518fece4316e3a56ed182cde7d19eab57f
camiloriosf <camilo@lastrev.com>
2021-09-21 14:46:49 -0300

    refactor to use next script

--- packages/component-library/src/components/FormMarketoEmbed/FormMarketoEmbed.tsx
+++ packages/component-library/src/components/FormMarketoEmbed/FormMarketoEmbed.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Script from 'next/script';
 import { Box } from '@material-ui/core';
 import styled from '@material-ui/system/styled';
 
@@ -12,33 +13,6 @@ export interface FormMarketoEmbedProps {
   baseUrl: string;
   munchkinId: string;
   formId: string;
-}
-
-interface UseMarketoHookProps extends FormMarketoEmbedProps {
-  callback: Function;
-}
-
-import { useState, useEffect } from 'react';
-
-function appendScript(baseUrl: string, setScriptLoaded: React.Dispatch<React.SetStateAction<boolean>>) {
-  if (window.MktoForms2) return setScriptLoaded(true);
-
-  const script = document.createElement('script');
-  script.src = `${baseUrl}/js/forms2/js/forms2.min.js`;
-  script.onload = () => (window.MktoForms2 ? setScriptLoaded(true) : null);
-  document.body.appendChild(script);
-}
-
-function useMarketo({ baseUrl, munchkinId, formId, callback }: UseMarketoHookProps) {
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-
-  useEffect(() => {
-    if (scriptLoaded) {
-      window.MktoForms2.loadForm(baseUrl, munchkinId, formId, callback);
-      return;
-    }
-    appendScript(baseUrl, setScriptLoaded);
-  }, [scriptLoaded, baseUrl, munchkinId, formId, callback]);
 }
 
 /* 
@@ -61,15 +35,15 @@ function addFieldModifiers(data: any) {
 }
 
 export const FormMarketoEmbed = ({ baseUrl, munchkinId, formId }: FormMarketoEmbedProps) => {
-  useMarketo({
-    baseUrl,
-    munchkinId,
-    formId,
-    callback: addFieldModifiers
-  });
-
   return (
     <Root>
+      <Script
+        id="marketo"
+        src={`${baseUrl}/js/forms2/js/forms2.min.js`}
+        onLoad={() => {
+          window.MktoForms2.loadForm(baseUrl, munchkinId, formId, addFieldModifiers);
+        }}
+      />
       <Form id={`mktoForm_${formId}`} />
     </Root>
   );
