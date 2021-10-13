e2368a827f8046796e5a492f64c3dbce362aa79a
Jaime Morales <jaime@lastrev.com>
2021-10-13 15:05:23 -0700

    Rebuilding entry_ids_by_content_type from contentful each time an entry is updated (#200)

--- packages/contentful-webhook-handler/src/handlers.ts
+++ packages/contentful-webhook-handler/src/handlers.ts
@@ -1,46 +1,94 @@
 import LastRevAppConfig from '@last-rev/app-config';
 import { Handlers } from './types';
 import Redis from 'ioredis';
+import { createClient } from 'contentful';
 import { updateAllPaths } from '@last-rev/contentful-path-util';
 import { createContext, createLoaders } from '@last-rev/graphql-contentful-helpers';
+import { Entry } from 'contentful';
 
 export const createRedisHandlers = (config: LastRevAppConfig): Handlers => {
-  const client = new Redis({
+  const redis = new Redis({
     ...config.redis,
     keyPrefix: `${config.contentful.spaceId}:${config.contentful.env}:`
   });
+
+  const contentfulPreviewClient = createClient({
+    space: config.contentful.spaceId,
+    accessToken: config.contentful.contentPreviewToken,
+    environment: config.contentful.env,
+    host: 'preview.contentful.com'
+  });
+
+  const contentfulProdClient = createClient({
+    space: config.contentful.spaceId,
+    accessToken: config.contentful.contentDeliveryToken,
+    environment: config.contentful.env,
+    host: 'cdn.contentful.com'
+  });
+
+  const refreshEntriesByContentType = async (contentTypeId: string, isPreview: boolean) => {
+    const client = isPreview ? contentfulPreviewClient : contentfulProdClient;
+    const setKey = `${isPreview ? 'preview' : 'production'}:entry_ids_by_content_type:${contentTypeId}`;
+
+    const makeRequest = async (skip: number = 0, existing: Entry<any>[] = []): Promise<Entry<any>[]> => {
+      const results = await client.getEntries({
+        content_type: contentTypeId,
+        skip,
+        limit: 1000,
+        select: 'sys.id'
+      });
+
+      const { items = [], total } = results;
+
+      existing.push(...items);
+      if (existing.length === total) {
+        return existing;
+      }
+      return makeRequest(skip + 1000, existing);
+    };
+
+    const entries = await makeRequest();
+    const entryIds = entries.map((entry) => entry.sys.id);
+
+    console.log('deleting', setKey);
+    console.log('adding', entryIds);
+
+    await redis
+      .multi()
+      .del(setKey)
+      .sadd(setKey, ...entryIds)
+      .exec();
+  };
 
   return {
     entry: async (command) => {
       const { data, isPreview } = command;
       const key = `${isPreview ? 'preview' : 'production'}:entries:${data.sys.id}`;
-      const setKey = `${isPreview ? 'preview' : 'production'}:entry_ids_by_content_type:${data.sys.contentType.sys.id}`;
       if (command.action === 'update') {
-        await client.set(key, JSON.stringify(data));
-        await client.sadd(setKey, data.sys.id);
+        await redis.set(key, JSON.stringify(data));
       } else if (command.action === 'delete') {
-        await client.del(key);
-        await client.srem(setKey, data.sys.id);
+        await redis.del(key);
       }
+      await refreshEntriesByContentType(data.sys.contentType.sys.id, isPreview);
     },
     asset: async (command) => {
       const { data, isPreview } = command;
       const key = `${isPreview ? 'preview' : 'production'}:assets:${data.sys.id}`;
       if (command.action === 'update') {
-        await client.set(key, JSON.stringify(data));
+        await redis.set(key, JSON.stringify(data));
       } else if (command.action === 'delete') {
-        await client.del(key);
+        await redis.del(key);
       }
     },
     contentType: async (command) => {
       const { data, isPreview } = command;
       const key = `${isPreview ? 'preview' : 'production'}:content_types`;
       if (command.action === 'update') {
-        await client.hset(key, {
+        await redis.hset(key, {
           [data.sys.id]: JSON.stringify(data)
         });
       } else if (command.action === 'delete') {
-        await client.hdel(key, data.sys.id);
+        await redis.hdel(key, data.sys.id);
       }
     },
     paths: async (updateForPreview, updateForProd) => {
