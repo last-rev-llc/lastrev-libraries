08ef555254e03c4b3a4c9bb45669bd4143fb394d
Maximiliano Techera <maxi.techerag@gmail.com>
2021-10-22 13:49:12 -0300

    Improve Card link wrapping (#220)

---
+++ .changeset/flat-islands-matter.md
@@ -0,0 +1,5 @@
+---
+'@last-rev/component-library': patch
+---
+
+Improve Card link wrapping, Fix Collection double container
--- packages/component-library/src/components/Card/Card.tsx
+++ packages/component-library/src/components/Card/Card.tsx
@@ -15,7 +15,6 @@ import { MediaProps } from '../Media/Media.types';
 import Link, { LinkProps } from '../Link';
 import Text, { RichText } from '../Text';
 import sidekick from '../../utils/sidekick';
-import ConditionalWrapper from '../ConditionalWrapper';
 
 export interface CardProps extends MuiCardProps {
   __typename: string;
@@ -32,84 +31,86 @@ export interface CardProps extends MuiCardProps {
 
 export interface CardOverrides {}
 
-export const Card = ({ media, title, subtitle, body, link, actions, variant, loading, sidekickLookup }: CardProps) => {
+export const Card = ({
+  media,
+  title,
+  subtitle,
+  body,
+  link,
+  actions,
+  variant,
+  loading,
+  sidekickLookup,
+  ...props
+}: CardProps) => {
   return (
     <ErrorBoundary>
-      <ConditionalWrapper
-        condition={!!link}
-        wrapper={(children: any) => (
-          <CardLink noLinkStyle {...(link as any)} sidekickLookup={{}}>
-            {children}
-          </CardLink>
-        )}
-      >
-        <Root variant={variant} data-testid="Card" {...sidekick(sidekickLookup)}>
-          {media || loading ? (
-            <CardMedia sx={{ display: 'flex', justifyContent: 'center' }}>
-              {!loading ? (
+      <Root variant={variant} data-testid="Card" {...sidekick(sidekickLookup)} {...(props as any)}>
+        {!!link ? <CardLink noLinkStyle href={link?.href} /> : null}
+        {media || loading ? (
+          <CardMedia sx={{ display: 'flex', justifyContent: 'center' }}>
+            {!loading ? (
+              <Media
+                {...sidekick(sidekickLookup?.media)}
+                {...(Array.isArray(media) ? media[0] : media)}
+                testId="Card-media"
+              />
+            ) : (
+              <Skeleton>
                 <Media
                   {...sidekick(sidekickLookup?.media)}
                   {...(Array.isArray(media) ? media[0] : media)}
                   testId="Card-media"
                 />
-              ) : (
-                <Skeleton>
-                  <Media
-                    {...sidekick(sidekickLookup?.media)}
-                    {...(Array.isArray(media) ? media[0] : media)}
-                    testId="Card-media"
-                  />
-                </Skeleton>
-              )}
-            </CardMedia>
-          ) : null}
-          {!loading && (title || subtitle || body || actions) ? (
-            <CardContent>
-              {title ? (
-                <Typography {...sidekick(sidekickLookup?.title)} variant="h3" component="h3" data-testid="Card-title">
-                  {title}
-                </Typography>
-              ) : null}
-              {subtitle ? (
-                <Typography
-                  {...sidekick(sidekickLookup?.subtitle)}
-                  variant="h4"
-                  component="h4"
-                  data-testid="Card-subtitle"
-                >
-                  {subtitle}
-                </Typography>
-              ) : null}
-              {body ? <Text sidekickLookup={sidekickLookup?.body} body={body} data-testid="Card-body" /> : null}
-              {actions?.length ? (
-                <CardActions {...sidekick(sidekickLookup?.actions)} data-testid="Card-actions">
-                  {actions?.map((link) => (
-                    <Link key={link.id} {...link} />
-                  ))}
-                </CardActions>
-              ) : null}
-            </CardContent>
-          ) : null}
-          {loading ? (
-            <CardContent>
-              <Typography variant="h3" component="h3">
-                <Skeleton width="100%" />
-              </Typography>
-              <Typography variant="h4" component="h4">
-                <Skeleton width="100%" />
-                <br />
-                <Skeleton width="100%" />
-              </Typography>
-              <Skeleton>
-                {body ? <Text sidekickLookup={sidekickLookup?.body} body={body} data-testid="Card-body" /> : null}
               </Skeleton>
-              <CardActions>
-                <Skeleton width={50} />
+            )}
+          </CardMedia>
+        ) : null}
+        {!loading && (title || subtitle || body || actions) ? (
+          <CardContent>
+            {title ? (
+              <Typography {...sidekick(sidekickLookup?.title)} variant="h3" component="h3" data-testid="Card-title">
+                {title}
+              </Typography>
+            ) : null}
+            {subtitle ? (
+              <Typography
+                {...sidekick(sidekickLookup?.subtitle)}
+                variant="h4"
+                component="h4"
+                data-testid="Card-subtitle">
+                {subtitle}
+              </Typography>
+            ) : null}
+            {body ? <Text sidekickLookup={sidekickLookup?.body} body={body} data-testid="Card-body" /> : null}
+            {actions?.length ? (
+              <CardActions {...sidekick(sidekickLookup?.actions)} data-testid="Card-actions">
+                {actions?.map((link) => (
+                  <Link key={link.id} {...link} />
+                ))}
               </CardActions>
-            </CardContent>
-          ) : null}
-        </Root>
-      </ConditionalWrapper>
+            ) : null}
+          </CardContent>
+        ) : null}
+        {loading ? (
+          <CardContent>
+            <Typography variant="h3" component="h3">
+              <Skeleton width="100%" />
+            </Typography>
+            <Typography variant="h4" component="h4">
+              <Skeleton width="100%" />
+              <br />
+              <Skeleton width="100%" />
+            </Typography>
+            <Skeleton>
+              {body ? <Text sidekickLookup={sidekickLookup?.body} body={body} data-testid="Card-body" /> : null}
+            </Skeleton>
+            <CardActions>
+              <Skeleton width={50} />
+            </CardActions>
+          </CardContent>
+        ) : null}
+      </Root>
     </ErrorBoundary>
   );
 };
@@ -119,15 +120,21 @@ const Root = styled(MuiCard, {
   slot: 'Root',
   shouldForwardProp: (prop) => prop !== 'variant',
   overridesResolver: (_, styles) => [styles.root]
-})<MuiCardProps & {}>(() => ({}));
+})<MuiCardProps & {}>`
+  position: relative;
+`;
 
 const CardLink = styled(Link, {
   name: 'Card',
   slot: 'CardLink',
   shouldForwardProp: (prop) => prop !== 'variant',
-  overridesResolver: (_, styles) => [styles.root]
+  overridesResolver: (_, styles) => [styles.cardLink]
 })<LinkProps & {}>`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
 `;
 
 export default Card;
