48ff721d61fccad421f4615a5d20b7769835b60c
Anthony Whitley <justin@lastrev.com>
2021-10-28 15:25:23 -0400

    Added cypress tests for Hero; Updated Hero mock and storybook

--- packages/component-library/src/components/Hero/Hero.mock.ts
+++ packages/component-library/src/components/Hero/Hero.mock.ts
@@ -1,19 +1,21 @@
 import { lorem } from 'faker';
 import { capitalize } from 'lodash';
+import { HeroProps } from './Hero';
 import mockLink from '../Link/Link.mock';
-import { mediaMock } from '../Media/Media.mock';
 import mockTheme from '../../theme/mock.theme';
 
-export default {
+export default (): HeroProps => ({
+  id: 'hero',
+  __typename: 'Hero',
   variant: 'default',
   overline: capitalize(lorem.words(3)),
   title: capitalize(lorem.words(3)),
   subtitle: lorem.sentence(),
   image: {
-    ...mediaMock(),
     file: {
       url: 'https://images.ctfassets.net/m1b67l45sk9z/4Oj0gAEf0wFiPfg0R3QHqG/b1f835f3e380670cd6484e486c9816b1/it-gets-better.png?h=800'
-    }
+    },
+    title: lorem.sentence()
   },
   body: {
     // __typename: 'Text',
@@ -72,13 +74,13 @@ export default {
   },
   actions: [{ ...mockLink(), text: 'Hero CTA' }],
   background: {
-    ...mediaMock(),
     file: {
       url: 'https://i.picsum.photos/id/327/2800/800.jpg?hmac=lqhEpkLvfvBfoZSxszEf8pOTbitkmHpJmZsoQYcrWkI'
-    }
+    },
+    title: lorem.sentence()
   },
   backgroundColor: 'white',
   // contentHeight: 'xl',
   // contentWidth: 'xl',
   theme: [mockTheme]
-};
+});
---
+++ packages/component-library/src/components/Hero/Hero.spec.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import mount from '../../../cypress/mount';
+import Hero, { HeroProps } from './Hero';
+import mockContent from './Hero.mock';
+
+let mockedContent: HeroProps = { __typename: 'Hero', id: 'hero', title: 'test', theme: {} };
+
+
+beforeEach(() => {
+  mockedContent = { ...mockContent() };
+});
+
+describe('Hero', () => {
+  context('renders correctly', () => {
+    it('renders a hero with correct content', () => {
+      mount(<Hero {...mockedContent} />);
+      cy.get('[data-testid=Hero]').should('exist');
+      cy.get('[data-testid=Hero-title]').should('exist')
+        .and('have.text', mockedContent.title);
+
+      cy.get('[data-testid=Hero-subtitle]').should('exist').and('have.text', mockedContent.subtitle);
+      cy.get('[data-testid=Hero-body]').should('exist')
+        .and('have.text', mockedContent.body.json.content[0].content.map(c => c.content[0].content[0].value).join(''));
+
+      cy.get('[data-testid=Hero-background]').should('exist').and('have.attr', 'src', mockedContent.background.file.url);
+      cy.get('[data-testid=Hero-image]').should('exist')
+        .and('have.attr', 'src', (Array.isArray(mockedContent.image) ? mockedContent.image[0] : mockedContent.image).file.url);
+
+      cy.contains(mockedContent.actions[0]?.text).should('exist');
+      cy.percySnapshot();
+    });
+
+    it('renders a hero with no title', () => {
+      mount(<Hero {...mockedContent} title={undefined} />);
+      cy.get('[data-testid=Hero]').should('exist');
+      cy.get('[data-testid=Hero-title]').should('not.exist');
+      cy.percySnapshot();
+    });
+
+    it('renders a hero with no subtitle', () => {
+      mount(<Hero {...mockedContent} subtitle={undefined} />);
+      cy.get('[data-testid=Hero]').should('exist');
+      cy.get('[data-testid=Hero-subtitle]').should('not.exist');
+      cy.percySnapshot();
+    });
+
+    it('renders a hero with no body', () => {
+      mount(<Hero {...mockedContent} body={undefined} />);
+      cy.get('[data-testid=Hero]').should('exist');
+      cy.get('[data-testid=Hero-body]').should('not.exist');
+      cy.percySnapshot();
+    });
+
+    it('renders a hero with no background image', () => {
+      mount(<Hero {...mockedContent} background={undefined} />);
+      cy.get('[data-testid=Hero]').should('exist');
+      cy.get('[data-testid=Hero-background]').should('not.exist');
+      cy.percySnapshot();
+    });
+
+    it('renders a hero with no image', () => {
+      mount(<Hero {...mockedContent} image={undefined} />);
+      cy.get('[data-testid=Hero]').should('exist');
+      cy.get('[data-testid=Hero-image]').should('not.exist');
+      cy.percySnapshot();
+    });
+
+    it('renders a hero with no cta button', () => {
+      mount(<Hero {...mockedContent} actions={undefined} />);
+      cy.get('[data-testid=Hero]').should('exist');
+      cy.contains(mockedContent.actions[0]?.text).should('not.exist');
+      cy.percySnapshot();
+    });
+  });
+});
