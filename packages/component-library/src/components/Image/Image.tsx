d1b24fd4063b393888c836ba423f471093a9f939
camiloriosf <camilo@lastrev.com>
2021-11-17 19:16:01 -0300

    Add unoptimized flag to preload

--- packages/component-library/src/components/Image/Image.tsx
+++ packages/component-library/src/components/Image/Image.tsx
@@ -57,7 +57,9 @@ const Image = React.forwardRef<any, ImageProps>(
                   rel="preload"
                   href={src}
                   // @ts-ignore
-                  imagesrcset={getImgSrcTag({ src, numColumns: columns, returnAttrsType: 'Obj', q })?.srcSet}
+                  imagesrcset={
+                    getImgSrcTag({ src, numColumns: columns, returnAttrsType: 'Obj', q, unoptimized })?.srcSet
+                  }
                   as="image"
                   media={media}
                 />
