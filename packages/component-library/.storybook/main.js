2a50ca8a38d255383e7a735f7f6c95f270598a40
Max Techera <maxi.techerag@gmail.com>
2021-11-30 14:00:53 -0300

    Update storybook webpack config for framer-motion issues

--- packages/component-library/.storybook/main.js
+++ packages/component-library/.storybook/main.js
@@ -24,6 +24,11 @@ module.exports = {
     '@storybook/addon-storysource'
   ],
   webpackFinal: async (config) => {
+    config.module.rules.push({
+      type: 'javascript/auto',
+      test: /\.mjs$/,
+      include: /node_modules/
+    });
     return {
       ...config,
       resolve: {
