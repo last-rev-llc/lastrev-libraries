759ab3e35c35a0f685c9fb709cba8fac3005b02f
Dano Alexander <altruity@gmail.com>
2022-01-12 16:14:56 +0300

    Cypress test config (#4)
    * IAS-87 Cypress test config
    
    * Small corrections
    
    * Remove comment
    
    * Post-review edits

--- packages/components/babel.config.js
+++ packages/components/babel.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-optional-chaining']
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-nullish-coalescing-operator'
+  ],
+  exclude: /node_modules/,
+  env: {
+    test: {
+      plugins: ['istanbul']
+    }
+  }
 };
---
+++ packages/components/cypress/mount.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { mount as ogMount } from '@cypress/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+
+// TODO: Add support for SCSS loader in Cypress
+// import * as components from '@last-rev/component-library/dist';
+import Header from '@last-rev/component-library/dist/components/Header/Header';
+import Text from '@last-rev/component-library/dist/components/Text/Text';
+import Link from '@last-rev/component-library/dist/components/Link/Link';
+import Media from '@last-rev/component-library/dist/components/Media/Media';
+import Card from '@last-rev/component-library/dist/components/Card/Card';
+import Collection from '@last-rev/component-library/dist/components/Collection/Collection';
+import CollectionCarousel from '@last-rev/component-library/dist/components/CollectionCarousel/CollectionCarousel';
+import CollectionAccordion from '@last-rev/component-library/dist/components/CollectionAccordion/CollectionAccordion';
+import CollectionFiltered from '@last-rev/component-library/dist/components/CollectionFiltered/CollectionFiltered';
+import Accordion from '@last-rev/component-library/dist/components/Accordion/Accordion';
+import Section from '@last-rev/component-library/dist/components/Section/Section';
+import BackToTop from '@last-rev/component-library/dist/components/BackToTop/BackToTop';
+import FormMarketoEmbed from '@last-rev/component-library/dist/components/FormMarketoEmbed/FormMarketoEmbed';
+import Hero from '@last-rev/component-library/dist/components/Hero/Hero';
+import Image from '@last-rev/component-library/dist/components/Image/Image';
+import MailchimpForm from '@last-rev/component-library/dist/components/MailchimpForm/MailchimpForm';
+import SEO from '@last-rev/component-library/dist/components/SEO/SEO';
+import { ContentModuleProvider } from '@last-rev/component-library/dist/components/ContentModule/ContentModuleContext';
+import theme from '../src/theme';
+
+const components = {
+  Accordion,
+  Header,
+  Text,
+  Link,
+  Media,
+  Card,
+  Collection,
+  'Collection:carousel': CollectionCarousel,
+  'Collection:accordion': CollectionAccordion,
+  'Collection:filtered': CollectionFiltered,
+  'Collection:carousel-large': CollectionCarousel,
+  'Collection:carousel-small': CollectionCarousel,
+  Section,
+  BackToTop,
+  Hero,
+  'LeadForm:marketo-default': FormMarketoEmbed,
+  Image,
+  MailchimpForm,
+  SEO
+};
+
+const mount = (component, options) =>
+  ogMount(
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <ContentModuleProvider contentMapping={components}>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" />
+          <CssBaseline />
+          {component}
+        </ContentModuleProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>,
+    options
+  );
+
+export default mount;
