02900861dece3cb0e0bb0f58de91f7dd9493f49d
Jaime Morales <jaime.raul.morales@gmail.com>
2022-02-08 12:36:20 -0800

    added articles to CategoryArticle extension (#39)

--- packages/graphql-extensions/src/CategoryArticle.ts
+++ packages/graphql-extensions/src/CategoryArticle.ts
@@ -1,17 +1,38 @@
 import gql from 'graphql-tag';
+import { ApolloContext } from '@last-rev/types';
+import { getLocalizedField } from '@last-rev/graphql-contentful-core';
 
 export const typeDefs = gql`
   extend type CategoryArticle {
     header: Header
     footer: Content
+    subCategories: [CategoryArticle]
+    articles: [Article]
   }
 `;
 
 export const mappers: any = {
   CategoryArticle: {
+    CategoryArticle: {
+      subCategories: 'categoryItems',
+      articles: async (category: any, _: never, ctx: ApolloContext) => {
+        const allArticles = await ctx.loaders.entriesByContentTypeLoader.load({
+          id: 'article',
+          preview: !!ctx.preview
+        });
+        const articles = (allArticles || []).filter((article: any) => {
+          const categoryIds = (getLocalizedField(article.fields, 'categories', ctx) || []).map(
+            (category: any) => category?.sys.id
+          );
+          return categoryIds.includes(category.sys.id);
+        });
+
+        return articles;
+      }
+    },
     Link: {
       href: 'slug',
-      text: 'title',
+      text: 'title'
       // variant: () => {
       //   return 'button-contained';
       // }
