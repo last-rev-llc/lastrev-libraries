3d229fd05a65b4a3bab73c6047cd82054d6bf044
Anthony Whitley <justin@lastrev.com>
2022-02-09 12:17:47 -0500

    Updated tsconfig; Removed ts errors in tests; Added new object parameter changes; Added new types

--- packages/contentful-sidekick-util/src/types.ts
+++ packages/contentful-sidekick-util/src/types.ts
@@ -4,3 +4,17 @@ export type SidekickData = {
   'data-csk-entry-type'?: string;
   'data-csk-entry-display-text'?: string;
 };
+
+export type ContentData = {
+  contentId?: string,
+  fieldName?: string,
+  contentTypeId?: string,
+  displayText?: string
+};
+
+export interface SidekickProps {
+  contentData?: string | ContentData,
+  fieldName?: string,
+  contentTypeId?: string,
+  displayText?: string
+}
