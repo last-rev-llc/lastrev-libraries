9c87091df377d63fb3e5bf4b25d9b9d467ac25e6
Cameron Taylor <50385537+ct3685@users.noreply.github.com>
2022-03-09 11:59:45 -0500

    Adding GTM and Redirects to codebase (#52)
    Co-authored-by: Adam Harris <adam@lastrev.com>

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -12,8 +12,30 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://images.ctfassets.net" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+              `
+            }}
+          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              title="GTM-NOSCRIPT"
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
