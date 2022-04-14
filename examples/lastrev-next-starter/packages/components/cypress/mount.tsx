e49da862c370304c6af41ec72131d5c7247783c1
Max Techera <maxi.techerag@gmail.com>
2022-04-14 16:42:13 -0300

    GActions optimization and cleanup

---
+++ examples/lastrev-next-starter/packages/components/cypress/mount.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount as ogMount } from '@cypress/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import { ContentModuleProvider } from '@last-rev/component-library/dist/components/ContentModule/ContentModuleContext';
+import theme from '../src/theme';
+import contentMapping from '../src/contentMapping';
+import '@last-rev/component-library/dist/styles.css';
+
+const mount = (component: any, options: any) =>
+  ogMount(
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <ContentModuleProvider contentMapping={contentMapping}>
+          <>
+            <base href="http://localhost:8080/" />
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" />
+            <CssBaseline />
+            {component}
+          </>
+        </ContentModuleProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>,
+    options
+  );
+
+export default mount;
