a224d9fbd542915ba210cc572f4e70f0602ccd68
opassion <13392489+opassion@users.noreply.github.com>
2022-08-02 13:40:05 -0400

    Update test mock

--- packages/sitemap-generator/src/tests/sitemap-generator.test.ts
+++ packages/sitemap-generator/src/tests/sitemap-generator.test.ts
@@ -10,9 +10,9 @@ describe('sitemap-generator', () => {
     await generateSitemap(sitemapMock(), './out');
 
     const sitemap = fs.readFileSync(path.resolve(__dirname, '../../out/sitemap.xml'), { encoding: 'utf8' });
-    const page1 = fs.readFileSync(path.resolve(__dirname, '../../out/page1'), { encoding: 'utf8' });
-    const page2 = fs.readFileSync(path.resolve(__dirname, '../../out/page2'), { encoding: 'utf8' });
-    const page3 = fs.readFileSync(path.resolve(__dirname, '../../out/page3'), { encoding: 'utf8' });
+    const page1 = fs.readFileSync(path.resolve(__dirname, '../../out/page1.xml'), { encoding: 'utf8' });
+    const page2 = fs.readFileSync(path.resolve(__dirname, '../../out/page2.xml'), { encoding: 'utf8' });
+    const page3 = fs.readFileSync(path.resolve(__dirname, '../../out/page3.xml'), { encoding: 'utf8' });
 
     expect(validateSchema('./schemas/siteindex.xsd', sitemap)).toBeTruthy();
     expect(validateSchema('./schemas/sitemap.xsd', page1)).toBeTruthy();
@@ -22,15 +22,15 @@ describe('sitemap-generator', () => {
     expect(sitemap).toMatchInlineSnapshot(`
       "<sitemapindex xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\">
         <sitemap>
-          <loc>https://site.com/page1</loc>
+          <loc>https://site.com/page1.xml</loc>
           <lastmod>2022-02-08</lastmod>
         </sitemap>
         <sitemap>
-          <loc>https://site.com/page2</loc>
+          <loc>https://site.com/page2.xml</loc>
           <lastmod>2022-02-08</lastmod>
         </sitemap>
         <sitemap>
-          <loc>https://site.com/page3</loc>
+          <loc>https://site.com/page3.xml</loc>
           <lastmod>2022-02-08</lastmod>
         </sitemap>
       </sitemapindex>"
@@ -38,15 +38,15 @@ describe('sitemap-generator', () => {
     expect(page1).toMatchInlineSnapshot(`
       "<urlset xmlns:xsi=\\"http://www.w3.org/2001/XMLSchema-instance\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xsi:schemaLocation=\\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd\\" xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\">
         <url>
-          <loc>https://site.com/page1</loc>
+          <loc>https://site.com/page1-a</loc>
           <lastmod>2022-02-01</lastmod>
         </url>
         <url>
-          <loc>https://site.com/page1</loc>
+          <loc>https://site.com/page1-b</loc>
           <lastmod>2022-02-03</lastmod>
         </url>
         <url>
-          <loc>https://site.com/page1</loc>
+          <loc>https://site.com/page1-c</loc>
           <lastmod>2022-02-08</lastmod>
         </url>
       </urlset>"
@@ -54,15 +54,15 @@ describe('sitemap-generator', () => {
     expect(page2).toMatchInlineSnapshot(`
       "<urlset xmlns:xsi=\\"http://www.w3.org/2001/XMLSchema-instance\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xsi:schemaLocation=\\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd\\" xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\">
         <url>
-          <loc>https://site.com/page2</loc>
+          <loc>https://site.com/page2-a</loc>
           <lastmod>2022-02-01</lastmod>
         </url>
         <url>
-          <loc>https://site.com/page2</loc>
+          <loc>https://site.com/page2-b</loc>
           <lastmod>2022-02-03</lastmod>
         </url>
         <url>
-          <loc>https://site.com/page2</loc>
+          <loc>https://site.com/page2-c</loc>
           <lastmod>2022-02-08</lastmod>
         </url>
       </urlset>"
@@ -70,15 +70,15 @@ describe('sitemap-generator', () => {
     expect(page3).toMatchInlineSnapshot(`
       "<urlset xmlns:xsi=\\"http://www.w3.org/2001/XMLSchema-instance\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xsi:schemaLocation=\\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd\\" xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\">
         <url>
-          <loc>https://site.com/page3</loc>
+          <loc>https://site.com/page3-a</loc>
           <lastmod>2022-02-01</lastmod>
         </url>
         <url>
-          <loc>https://site.com/page3</loc>
+          <loc>https://site.com/page3-b</loc>
           <lastmod>2022-02-03</lastmod>
         </url>
         <url>
-          <loc>https://site.com/page3</loc>
+          <loc>https://site.com/page3-c</loc>
           <lastmod>2022-02-08</lastmod>
         </url>
       </urlset>"
@@ -94,6 +94,6 @@ function validateSchema(schemaPath: string, file: any) {
   const schemaDoc = libxmljs.parseXml(schemaFile);
 
   // Perform validation
-  const isValid = xmlDoc.validate(schemaDoc); 
+  const isValid = xmlDoc.validate(schemaDoc);
   return isValid;
 }
--- packages/sitemap-generator/src/tests/sitemap.mock.ts
+++ packages/sitemap-generator/src/tests/sitemap.mock.ts
@@ -5,18 +5,22 @@ const sitemapEntry = (loc: string, day: string): SitemapEntry => ({
   lastmod: `2022-02-${day}`
 });
 
-const sitemapPage = (loc: string, filename: string): SitemapPage => ({
-  loc,
+const sitemapPage = (domain: string, loc: string): SitemapPage => ({
+  loc: `${domain}/${loc}.xml`,
   lastmod: '2022-02-08',
-  filename,
-  entries: [sitemapEntry(loc, '01'), sitemapEntry(loc, '03'), sitemapEntry(loc, '08')]
+  filename: `${loc}.xml`,
+  entries: [
+    sitemapEntry(`${domain}/${loc}-a`, '01'),
+    sitemapEntry(`${domain}/${loc}-b`, '03'),
+    sitemapEntry(`${domain}/${loc}-c`, '08')
+  ]
 });
 
 const sitemapMock = (): Sitemap => ({
   pages: [
-    sitemapPage('https://site.com/page1', 'page1'),
-    sitemapPage('https://site.com/page2', 'page2'),
-    sitemapPage('https://site.com/page3', 'page3')
+    sitemapPage('https://site.com', 'page1'),
+    sitemapPage('https://site.com', 'page2'),
+    sitemapPage('https://site.com', 'page3')
   ]
 });
 
