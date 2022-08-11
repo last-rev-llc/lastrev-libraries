d964a532dc8cd86059d4d58aee52939fb4c9e5b0
Jaime Morales <jaime@lastrev.com>
2022-08-11 11:10:26 -0700

    removed comment

--- examples/lastrev-next-starter/packages/utils/src/client.tsx
+++ examples/lastrev-next-starter/packages/utils/src/client.tsx
@@ -5,7 +5,6 @@ const URL =
   process.env.STAGE === 'build' || !process.env.DEPLOY_URL
     ? 'http://localhost:5000/graphql'
     : `${process.env.DEPLOY_URL}/api/graphql`;
-console.log('graphql URL', URL);
 const sdk = getSdk(new GraphQLClient(URL));
 
 export default sdk;
