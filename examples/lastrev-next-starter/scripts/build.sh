6ea5c3f416b24e32f2dc543f02dbaa74f0aba3f3
Jaime Morales <jaime@lastrev.com>
2022-08-16 12:51:11 -0700

    added back cms sync and fs strategy, based on GRAPHQL_RUNNER_STRATEGY var

--- examples/lastrev-next-starter/scripts/build.sh
+++ examples/lastrev-next-starter/scripts/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -a
+source .env || echo "No .env file found"
+set +a
 function cleanup() {
     rv=$?
-    yarn gql:pm2:kill
+    bash "$PWD/scripts/post_build.sh"
+    if [[ "$rv" != "0" ]]; then
+        echo "Build failed."
+        yarn pm2 logs --nostream --lines=1000
+    fi
     exit $rv
 }
 
@@ -9,6 +16,8 @@ trap "cleanup" EXIT
 
 yarn propagate:env
 
+bash "$PWD/scripts/pre_build.sh"
+
 # Run build and cleanup pm2 if it fails
 echo "Building..."
-STAGE=build yarn turbo:build --output-logs=new-only $1 || echo "Build failed."
+STAGE=build yarn turbo:build --output-logs=new-only $1
---
+++ examples/lastrev-next-starter/scripts/pre_build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# If a graphql strategy is not defined, default to fs,
+# if redis stratedgy is fs, then prepare for and then run cms sync,
+if [[ "${GRAPHQL_RUNNER_STRATEGY}" == "fs" ]] || [[ -z "${GRAPHQL_RUNNER_STRATEGY}" ]]; then
+
+    # This script ensures cms-sync folder is not ignored during turbo build
+    # The fingerprint hash that Turbo uses ignores anything ignored in the .gitignore file
+    # We want the cms-sync data to be part of the hash for the graphql-runner package
+    # This way when content changes packages depending on graphql-runner will be invalidated
+    echo "Removing cms-sync from graphql-runner .gitignore"
+
+    RUNNER_FOLDER=$PWD/packages/graphql-runner
+    GITIGNORE=$RUNNER_FOLDER/.gitignore
+    NOT_GITIGNORE=$RUNNER_FOLDER/!gitignore
+
+    echo $GITIGNORE
+
+    if [ -f "$GITIGNORE" ]; then
+        echo ".gitignore exists."
+    else
+        echo ".gitignore does not exist. Trying to fix..."
+        if [ -f "$NOT_GITIGNORE" ]; then
+            mv ${NOT_GITIGNORE} ${GITIGNORE} || echo "Failed to fix from !gitignore"
+        else
+            echo "!gitignore does not exist. Please fix manually."
+            echo "Web build cache hash will not include cms-sync folder"
+        fi
+    fi
+
+
+    if [ -f "$GITIGNORE" ]; then
+        mv ${GITIGNORE} ${NOT_GITIGNORE} 
+        echo "Renamed .gitignore to !gitignore"
+    fi
+
+    echo "Syncing CMS data.."
+    yarn turbo:sync:cms --output-logs=new-only
+else
+    echo "Graphql strategy is '${GRAPHQL_RUNNER_STRATEGY}'. Skipping cms sync."
+fi
