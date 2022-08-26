63106231e5f1526bc5430013108927d8eaa306a2
Max Techera <maxi.techerag@gmail.com>
2022-08-26 16:57:12 -0300

    Turn video false

--- packages/component-library/cypress.config.ts
+++ packages/component-library/cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 
   fileServerFolder: 'public',
   videoUploadOnPasses: false,
+  video: false,
   component: {
     specPattern: 'src/**/*spec.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
