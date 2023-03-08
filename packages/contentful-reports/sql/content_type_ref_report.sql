87a49e268c0e5dec957c193ab31a86500d837330
Jaime Morales <jaime@lastrev.com>
2023-03-08 20:38:48 +0100

    Fixed the content type reference report

--- packages/contentful-reports/sql/content_type_ref_report.sql
+++ packages/contentful-reports/sql/content_type_ref_report.sql
@@ -1,20 +1,13 @@
+-- a report showing what content types another type is allowed to be referenced by
 SELECT
-  ct1.name AS 'Content Type Name',
-  ct1.id AS 'Content Type ID',
-  f.name AS 'Field Name',
-  f.id AS 'Field ID',
-  ct2.name AS 'Allowed Type Name',
-  ct2.id AS 'Allowed Type ID'
+  ct2.name AS 'Content Type Name',
+  ct2.id AS 'Content Type ID',
+  COALESCE(GROUP_CONCAT(DISTINCT ct1.id), '') AS 'Types that can reference this type'
 FROM
   field_content_type_validations
   LEFT JOIN content_types ct1 ON field_content_type_validations.content_type_id = ct1.id
   LEFT JOIN content_types ct2 ON field_content_type_validations.link_content_type_id = ct2.id
-  LEFT JOIN fields f ON field_content_type_validations.field_id = f.id
 GROUP BY
-  ct1.id,
-  ct2.id,
-  f.id
+  ct2.id
 ORDER BY
-  ct1.name,
-  f.name,
-  ct2.name;
+  ct2.name
