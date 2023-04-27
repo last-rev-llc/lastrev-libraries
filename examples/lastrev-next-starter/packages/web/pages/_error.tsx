b0ab7ae2496b36b342424cdbcd4455212a478a89
Justin Whitley <51485419+ajwhitley@users.noreply.github.com>
2023-04-27 11:26:50 -0400

    Updated next-starter Sentry configuration (#629)

--- examples/lastrev-next-starter/packages/web/pages/_error.tsx
+++ examples/lastrev-next-starter/packages/web/pages/_error.tsx
@@ -1,24 +1,16 @@
 import NextErrorComponent from 'next/error';
-
 import * as Sentry from '@sentry/nextjs';
 
-const MyError = ({ statusCode, hasGetInitialPropsRun, err }: any) => {
-  if (!hasGetInitialPropsRun && err) {
-    // getInitialProps is not called in case of
-    // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
-    // err via _app.js so it can be captured
-    Sentry.captureException(err);
-    // Flushing is not required in this case as it only happens on the client
-  }
+const MyError = ({ statusCode }: any) => {
+  // If you're using a Nextjs version prior to 12.2.1, uncomment this to
+  // compensate for https://github.com/vercel/next.js/issues/8592
+  // Sentry.captureUnderscoreErrorException(props);
 
   return <NextErrorComponent statusCode={statusCode} />;
 };
 
-MyError.getInitialProps = async ({ res, err, asPath, req }: any) => {
-  const errorInitialProps: any = await NextErrorComponent.getInitialProps({
-    res,
-    err
-  } as any);
+MyError.getInitialProps = async (contextData: any) => {
+  const { res, err, asPath } = contextData;
 
   const statusCode = res?.statusCode ?? err?.statusCode;
 
@@ -26,45 +18,12 @@ MyError.getInitialProps = async ({ res, err, asPath, req }: any) => {
     console.log(`Handle 404 error on path ${asPath}`);
     return { statusCode };
   }
-  // Workaround for https://github.com/vercel/next.js/issues/8592, mark when
-  // getInitialProps has run
-  errorInitialProps.hasGetInitialPropsRun = true;
 
-  // Running on the server, the response object (`res`) is available.
-  //
-  // Next.js will pass an err on the server if a page's data fetching methods
-  // threw or returned a Promise that rejected
-  //
-  // Running on the client (browser), Next.js will provide an err if:
-  //
-  //  - a page's `getInitialProps` threw or returned a Promise that rejected
-  //  - an exception was thrown somewhere in the React lifecycle (render,
-  //    componentDidMount, etc) that was caught by Next.js's React Error
-  //    Boundary. Read more about what types of exceptions are caught by Error
-  //    Boundaries: https://reactjs.org/docs/error-boundaries.html
+  // In case this is running in a serverless function, await this in order to give Sentry
+  // time to send the error before the lambda exits
+  await Sentry.captureUnderscoreErrorException(contextData);
 
-  if (err) {
-    if (err.message === 'Redirect') {
-      console.log(`Handle Redirect error on path ${asPath}`);
-    } else if (err.type === 'ChunkLoadError') {
-      console.log(`Handle ChunkLoadError error on path ${asPath}`);
-    } else {
-      Sentry.captureException(err);
-
-      // Flushing before returning is necessary if deploying to Vercel, see
-      // https://vercel.com/docs/platform/limits#streaming-responses
-      await Sentry.flush(2000);
-    }
-    return errorInitialProps;
-  }
-
-  // If this point is reached, getInitialProps was called without any
-  // information about what the error might be. This is unexpected and may
-  // indicate a bug introduced in Next.js, so record it in Sentry
-  Sentry.captureException(new Error(`_error.js getInitialProps missing data at path: ${asPath}`));
-  await Sentry.flush(2000);
-
-  return errorInitialProps;
+  return NextErrorComponent.getInitialProps(contextData);
 };
 
 export default MyError;
--- examples/lastrev-next-starter/packages/web/sentry.shared.config.js
+++ examples/lastrev-next-starter/packages/web/sentry.shared.config.js
@@ -4,7 +4,9 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-let lrns = require('../../lastrev.json');
+const lrns = require('../../lastrev.json');
+
+const knownErrors = ['Redirect', 'ChunkLoadError'];
 
 const sharedSentrySetup = () => {
   Sentry.setTag('lrns_version', lrns.app.version);
@@ -12,10 +14,21 @@ const sharedSentrySetup = () => {
   const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
   Sentry.init({
-    dsn: SENTRY_DSN
+    dsn: SENTRY_DSN,
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
     // that it will also get attached to your source maps
+    beforeSend(event, hint) {
+      let returnedEvent = event;
+      if (
+        (hint?.originalException?.message && knownErrors.includes(hint.originalException.message)) ||
+        (event?.exception?.values?.[0]?.type && event.exception.values[0].type === 'ChunkLoadError')
+      ) {
+        console.log(`Handle ${hint.originalException.message} error on path ${event?.request?.url}`);
+        returnedEvent = null;
+      }
+      return returnedEvent;
+    }
   });
 };
 
