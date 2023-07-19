7549ba1feb6e3b150956a686293fa0a816a7e9a6
Leonardo Massaroli <leonardo@lastrev.com>
2023-07-19 13:39:02 -0300

    parseBooleanEnvVar moved to utils package

--- examples/lastrev-next-starter/packages/functions/shared/utils.ts
+++
@@ -1,6 +0,0 @@
-export const parseBooleanEnvVar = (value: string) => {
-  if (!value) return false;
-  // values parsed as true: true, 1, yes, y, => ignore caps
-  const val = value.toString().toLowerCase();
-  return /^(true|1|yes|y)$/.test(val);
-};
