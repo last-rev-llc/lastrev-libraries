695dc134cd301eb341f47d59ba9a1d9530077322
Max Techera <maxi.techerag@gmail.com>
2023-09-18 18:25:07 -0300

    Make it compatible with the new starter types

--- examples/lastrev-next-starter-pnpm/apps/web/src/app/layout.tsx
+++ examples/lastrev-next-starter-pnpm/apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
 // TODO: Pull the SEO metadata from the Site settings in Contentful
+if (!process.env.DOMAIN) throw new Error('DOMAIN environment variable is required');
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.DOMAIN!),
   title: {
