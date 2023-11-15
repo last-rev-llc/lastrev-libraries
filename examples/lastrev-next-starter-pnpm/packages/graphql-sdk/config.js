0e23926e6cfd2a5fd4bd0a7accc848b393ffa040
Adam Harris <adam@lastrev.com>
2023-11-14 17:43:01 -0800

    Updated config

--- examples/lastrev-next-starter-pnpm/packages/graphql-sdk/config.js
+++ examples/lastrev-next-starter-pnpm/packages/graphql-sdk/config.js
@@ -30,8 +30,8 @@ const parseBooleanEnvVar = (value = '') => {
 
 const config = new LastRevAppConfig({
   cms: 'Contentful',
-  contentStrategy: 'fs',
-  // cmsCacheStrategy: 'redis',
+  contentStrategy: process.env.NODE_ENV !== 'production' ? 'cms' : undefined,
+  cmsCacheStrategy: 'redis',
   sites: [process.env.SITE],
   extensions,
   graphql: { port: 8888 },
