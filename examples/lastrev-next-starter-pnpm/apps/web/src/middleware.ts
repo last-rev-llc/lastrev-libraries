fbcd0d31251e1e1f3438ee845c77d9112d9c3585
Max Techera <maxi.techerag@gmail.com>
2023-11-20 14:04:45 -0300

    Add vercel deploy CSP

--- examples/lastrev-next-starter-pnpm/apps/web/src/middleware.ts
+++ examples/lastrev-next-starter-pnpm/apps/web/src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const cspHeader = `
-    default-src 'self'  localhost:8888 *.sentry.io  *.facebook.com vitals.vercel-insights.com *.hubapi.com *.hsforms.com *.hs-scripts.com *.hsforms.net *.hscollectedforms.net;
+    default-src 'self'  *.vercel.app localhost:8888 *.sentry.io  *.facebook.com vitals.vercel-insights.com *.hubapi.com *.hsforms.com *.hs-scripts.com *.hsforms.net *.hscollectedforms.net;
     style-src 'self' 'unsafe-inline'  *.sentry.io  ${process.env.NEXT_PUBLIC_MARKETO_BASE_URL} fonts.googleapis.com vitals.vercel-insights.com *.hs-scripts.com *.hsforms.net;
     script-src 'self' 'unsafe-inline' 'unsafe-eval' ${process.env.NEXT_PUBLIC_MARKETO_BASE_URL}  *.gstatic.com *.google.com *.google-analytics.com *.googletagmanager.com vitals.vercel-insights.com *.hs-analytics.net *.hs-banner.com *.hsadspixel.net *.hscollectedforms.net *.jquery.com *.hs-scripts.com *.hsforms.net;
     font-src 'self'  *.sentry.io  fonts.gstatic.com *.hs-scripts.com *.hsforms.net data:;
