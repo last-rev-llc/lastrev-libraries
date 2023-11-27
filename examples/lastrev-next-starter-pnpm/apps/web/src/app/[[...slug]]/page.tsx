d22642e1adf479df40e439508ac1a5362041fbda
Adam Harris <adam@lastrev.com>
2023-11-27 15:55:36 -0800

    revert page

--- examples/lastrev-next-starter-pnpm/apps/web/src/app/[[...slug]]/page.tsx
+++ examples/lastrev-next-starter-pnpm/apps/web/src/app/[[...slug]]/page.tsx
@@ -1,4 +1,3 @@
-// import { cache } from 'react';
 import { client } from '@graphql-sdk/client';
 import { join } from 'path';
 import { notFound } from 'next/navigation';
@@ -10,17 +9,17 @@ import { isPreview } from '@ui/utils/isPreview';
 import ContentModule from '@ui/ContentModule/ContentModule';
 
 const site = process.env.SITE;
+
 type Props = {
   params: { slug: string[] };
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export const revalidate = 300;
-const getPage = client.Page;
 
 export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
   const path = join('/', (params.slug || ['/']).join('/'));
-  const { data: pageData } = await getPage({ path, locale, preview: isPreview(), site });
+  const { data: pageData } = await client.Page({ path, locale, preview: isPreview(), site });
   if (!pageData?.page?.id) return {};
 
   const parentSEO = await parent;
@@ -30,7 +29,7 @@ export async function generateMetadata({ params }: Props, parent: ResolvingMetad
 
 // export async function generateStaticParams() {
 //   const locales = ['en-US', 'es-ES'];
-//   const paths = (await client.Paths({ locales, preview: isPreview(), site }))?.data?.paths;
+//   const paths = (await client.Paths({ locales, preview, site }))?.data?.paths;
 //   return paths?.map((p) => ({ slug: p.params.slug }));
 // }
 
@@ -39,18 +38,17 @@ export async function generateMetadata({ params }: Props, parent: ResolvingMetad
 
 const locale = 'en-US';
 
-export default async function Page({ params, searchParams }: Props) {
+export default async function Page({ params }: Props) {
   const path = join('/', (params.slug || ['/']).join('/'));
-  console.time('Load:' + path);
-  const { data: pageData, ...rest } = await client.Page({ path, locale, preview: isPreview(), site });
-  console.timeEnd('Load:' + path);
 
-  if (!(pageData?.page as any)?.path) {
+  const { data: pageData } = await client.Page({ path, locale, preview: isPreview(), site });
+
+  if (!pageData?.page) {
     return notFound();
   }
   return (
     <AppProvider>
-      <ContentModule {...pageData.page} searchParams={searchParams} />
+      <ContentModule {...pageData.page} />
     </AppProvider>
   );
 }
