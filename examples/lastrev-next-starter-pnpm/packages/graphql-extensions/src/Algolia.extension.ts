454f113564d49da2c24452ebcd0addfee299e4cb
Adam Harris <adam@lastrev.com>
2023-11-30 09:52:19 -0800

    add

--- examples/lastrev-next-starter-pnpm/packages/graphql-extensions/src/Algolia.extension.ts
+++ examples/lastrev-next-starter-pnpm/packages/graphql-extensions/src/Algolia.extension.ts
@@ -83,7 +83,7 @@ export const mappers = {
                 locale: ctx.locale || ctx.defaultLocale,
                 preview: !!ctx.preview,
                 title,
-                defaultSortField: pubDateTimestamp || 1 * -1,
+                defaultSortField: (pubDateTimestamp || 1) * -1,
                 pubDate,
                 categories,
                 pubDateTimestamp,
