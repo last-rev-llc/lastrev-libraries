c00604755f9b7190c290585eff623a8a9755226d
Adam Harris <adam@lastrev.com>
2023-12-03 16:51:33 -0800

    optimize picture images

--- examples/lastrev-next-starter-pnpm/packages/ui/src/Image/Image.tsx
+++ examples/lastrev-next-starter-pnpm/packages/ui/src/Image/Image.tsx
@@ -4,7 +4,39 @@ import NextImage from 'next/image';
 import { breakpointsMinMax } from '../ThemeRegistry/theme';
 import ErrorBoundary from '../ErrorBoundary';
 
+
 import type { ImageProps } from './Image.types';
+
+const calculateImageSizeForDiv = (divWidth:number, divHeight:number, originalWidth:number, originalHeight:number) => {
+  // Check if the original image is smaller than the div
+  if (originalWidth <= divWidth && originalHeight <= divHeight) {
+    return {
+      width: originalWidth,
+      height: originalHeight
+    };
+  }
+
+  // Calculate the aspect ratio of the div
+  const divAspectRatio = divWidth / divHeight;
+
+  // Calculate the aspect ratio of the original image
+  const originalAspectRatio = originalWidth / originalHeight;
+
+  let imageSize: { width: number; height: number } = {
+    width: divWidth,
+    height: divHeight
+  };
+
+  if (originalAspectRatio <= divAspectRatio) {
+    imageSize.width = divWidth;
+    imageSize.height = divWidth / originalAspectRatio;
+  } else {
+    imageSize.width = divHeight * originalAspectRatio;
+    imageSize.height = divHeight;
+  }
+
+  return imageSize;
+}
 
 const Image = React.forwardRef<any, ImageProps>(function Image(props, ref) {
   const {
@@ -31,101 +63,132 @@ const Image = React.forwardRef<any, ImageProps>(function Image(props, ref) {
   const imgContent = React.useMemo(() => {
     const isSVG = src?.endsWith('.svg');
 
+    
+    
     const generateSources = () => {
       const sources = [];
-      let maxWidth = 0; // Initialize maximum width to 0
-      const imageWidth = Number(width);
-      const imageHeight = Number(height);
+      let maxWidth = 0;
+      let maxHeight = 0;
+      const originalImageWidth = Number(width);
+      const originalImageHeight = Number(height);
 
       // Iterate over breakpoints to find the largest column width
       for (const breakpoint in breakpointsMinMax) {
-        // if (breakpointsMinMax.hasOwnProperty(breakpoint)) {
         const breakpointWidth = breakpointsMinMax[breakpoint]?.max;
         if (breakpointWidth && columns?.hasOwnProperty(breakpoint)) {
           const columnsValue = columns[breakpoint];
 
           if (!columnsValue) continue;
-          let widthValue = breakpointWidth / columnsValue;
+          let divWidth = breakpointWidth / columnsValue;
+          let divHeight = breakpointWidth / columnsValue;
 
-          if (aspectRatio === 'vertical' && breakpointWidth > 800) {
-            widthValue = (widthValue * 16) / 9;
+          if (aspectRatio === 'vertical' && breakpointWidth > breakpointsMinMax['sm']?.max) {
+            divHeight = (divWidth * 16) / 9;
+          } else if (aspectRatio === 'horizontal') {
+            divHeight = (divWidth * 9) / 16;
           }
 
-          if (widthValue > maxWidth) {
-            maxWidth = widthValue;
-          }
+          maxWidth = Math.max(maxWidth, divWidth);
+          maxHeight = Math.max(maxHeight, divHeight);
+          
         }
+      }
+
+      // Get the calculated image size using the calculateImageSize function
+      const imageSize = calculateImageSizeForDiv(maxWidth, maxHeight, originalImageWidth, originalImageHeight);
+
+      if (src?.includes('Walking')) {
+        console.log({imageSize, maxWidth, maxHeight, originalImageWidth, originalImageHeight})
       }
 
       // Iterate over breakpoints and generate sources
       for (const breakpoint in breakpointsMinMax) {
         const breakpointWidth = breakpointsMinMax[breakpoint]?.max;
-        const mediaCondition = `(min-width: ${breakpointsMinMax[breakpoint]?.min}px) and (max-width: ${
-          breakpointWidth - 0.001
-        }px)`;
+        // const mediaCondition = `(min-width: ${breakpointsMinMax[breakpoint]?.min}px) and (max-width: ${
+        //   breakpointWidth - 0.001
+        // }px)`;
 
         if (breakpointWidth && columns?.hasOwnProperty(breakpoint)) {
+          const mediaConditions = [];
+         
+          const min = breakpointsMinMax[breakpoint]?.min;
+          if (!!min) {
+            mediaConditions.push(`(min-width: ${min}px)`);
+          }
+         
+          
+          mediaConditions.push(`(max-width: ${breakpointWidth -  0.001}px)`);
+          
+
+          const mediaCondition = mediaConditions.join(" and ");
+
           const columnsValue = columns[breakpoint];
 
-          let widthValue = breakpointWidth / columnsValue;
+          let divWidth = breakpointWidth / columnsValue;
+          let divHeight = breakpointWidth / columnsValue;
 
-          if (aspectRatio === 'vertical' && breakpointWidth > 800) {
-            widthValue = (widthValue * 16) / 9;
+          if (aspectRatio === 'vertical' && breakpointWidth > breakpointsMinMax['sm']?.max) {
+            divHeight = (divWidth * 16) / 9;
+          } else if (aspectRatio === 'horizontal') {
+            divHeight = (divWidth * 9) / 16;
           }
 
-          if (widthValue <= maxWidth && imageWidth > widthValue) {
-            const source = `${src}?${imageWidth > imageHeight ? 'h' : 'w'}=${Math.round(widthValue)}&q=${quality}`;
+          if (divHeight < originalImageHeight) {
+            const source = `${src}?h=${Math.round(divHeight)}&q=${quality}`;
+         
             sources.push(
               <source
                 key={`avif-${src}-${breakpoint}`}
                 media={mediaCondition}
-                srcSet={`${source}&fm=avif`}
+                srcSet={`${source}&fm=avif, ${source}&fm=webp`}
                 type="image/avif"
               />
             );
-            sources.push(
-              <source
-                key={`webp-${src}-${breakpoint}`}
-                media={mediaCondition}
-                srcSet={`${source}&fm=webp`}
-                type="image/webp"
-              />
-            );
-          }
-        } else {
-          const source = `${src}?${imageWidth > imageHeight ? 'h' : 'w'}=${Math.round(maxWidth)}&q=${quality}`;
-          sources.push(
-            <source
-              key={`avif-${src}-${breakpoint}`}
-              media={mediaCondition}
-              srcSet={`${source}&fm=avif`}
-              type="image/avif"
-            />
-          );
-          sources.push(
-            <source
-              key={`webp-${src}-${breakpoint}`}
-              media={mediaCondition}
-              srcSet={`${source}&fm=webp`}
-              type="image/webp"
-            />
-          );
+
+            // sources.push(
+            //   <source
+            //     key={`webp-${src}-${breakpoint}`}
+            //     media={mediaCondition}
+            //     srcSet={`${source}&fm=webp`}
+            //     type="image/webp"
+            //   />
+            // );
+            }
         }
       }
 
+      let fallbackSrc = src;
+      if (originalImageHeight > maxHeight) {
+        fallbackSrc = `${src}?h=${Math.round(maxHeight)}&q=90`//${quality}`
+      }
+      
       sources.push(
-        <img
+        <NextImage
           key={`fallback-${src}`}
-          src={`${src}?${imageWidth > imageHeight ? 'h' : 'w'}=${Math.round(maxWidth)}&q=${quality}`}
+          src={fallbackSrc as string}
           alt={alt}
           className={className}
           ref={ref}
+          unoptimized={true}
           data-testid={testId}
           itemProp={itemProp}
           loading="lazy"
-          width={Math.round(maxWidth)} // Set fallback width
-          height={Math.round(imageHeight * (maxWidth / imageWidth))}
+          width={Math.round(imageSize.width)} 
+          height={Math.round(imageSize.height)}
         />
+
+        // <img
+        //   key={`fallback-${src}`}
+        //   src={fallbackSrc}
+        //   alt={alt}
+        //   className={className}
+        //   ref={ref}
+        //   data-testid={testId}
+        //   itemProp={itemProp}
+        //   loading="lazy"
+        //   width={Math.round(imageSize.width)} 
+        //   height={Math.round(imageSize.height)}
+        // />
       );
 
       return sources;
@@ -170,7 +233,7 @@ const Image = React.forwardRef<any, ImageProps>(function Image(props, ref) {
       );
     }
     return content;
-  }, [alt, className, columns, disableInlineSVG, height, imageProps, itemProp, priority, src, testId, width, ref]);
+  }, [alt, aspectRatio, quality, className, columns, disableInlineSVG, height, imageProps, itemProp, priority, src, testId, width, ref]);
 
   return <ErrorBoundary>{imgContent}</ErrorBoundary>;
 });
